2fdcb93b72449f6033a952c38b6243d376ba2012
timwlee <timothyl@adobe.com>
2025-04-02 12:59:51 -0700

    Update cards block

--- blocks/cards/cards.js
+++ blocks/cards/cards.js
@@ -1,68 +1,18 @@
 import { createOptimizedPicture } from '../../scripts/aem.js';
-import fetchHtml from '../story/story-cards-util.js';
 
-export default async function decorate(block) {
-  const isJSON = block.classList.contains('is-json');
-  const isStoryLinks = block.classList.contains('story-links');
-
-  async function fetchJson(link) {
-    const response = await fetch(link?.href);
-
-    if (response.ok) {
-      const jsonData = await response.json();
-      const data = jsonData?.data;
-      return data;
-    }
-    return 'an error occurred';
-  }
-
+export default function decorate(block) {
+  /* change to ul, li */
   const ul = document.createElement('ul');
-
-  if (isStoryLinks) {
-    const blockLinks = block.querySelectorAll('a');
-    blockLinks.forEach((link) => {
-      fetchHtml(link, ul);
+  [...block.children].forEach((row) => {
+    const li = document.createElement('li');
+    while (row.firstElementChild) li.append(row.firstElementChild);
+    [...li.children].forEach((div) => {
+      if (div.children.length === 1 && div.querySelector('picture')) div.className = 'cards-card-image';
+      else div.className = 'cards-card-body';
     });
-  }
-
-  if (isJSON) {
-    const link = block.querySelector('a');
-    const cardData = await fetchJson(link);
-    cardData.forEach((item) => {
-      const picture = createOptimizedPicture(item.image, item.title, false, [{ width: 320 }]);
-      picture.lastElementChild.width = '320';
-      picture.lastElementChild.height = '180';
-
-      const createdCard = document.createElement('li');
-
-      createdCard.innerHTML = `
-        <div class="cards-card-body">
-          // place inner HTML structure based on JSON response as needed
-        </div>
-      `;
-      ul.append(createdCard);
-    });
-  } else if (!isStoryLinks) {
-    [...block.children].forEach((row) => {
-      const li = document.createElement('li');
-      while (row.firstElementChild) li.append(row.firstElementChild);
-      [...li.children].forEach((div) => {
-        if (div.children.length === 1 && div.querySelector('picture')) {
-          div.className = 'cards-card-image';
-        } else if (div.children.length === 1 && div.querySelector('span')) {
-          div.className = 'cards-card-icon';
-        } else {
-          div.className = 'cards-card-body';
-        }
-      });
-
-      const image = li.querySelector('img');
-      image.setAttribute('loading', 'eager');
-
-      ul.append(li);
-    });
-  }
-
+    ul.append(li);
+  });
+  ul.querySelectorAll('picture > img').forEach((img) => img.closest('picture').replaceWith(createOptimizedPicture(img.src, img.alt, false, [{ width: '750' }])));
   block.textContent = '';
   block.append(ul);
 }
