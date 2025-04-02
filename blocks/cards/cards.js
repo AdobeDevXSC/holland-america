036d2b153fbd7a48eac09f5e5f1c390a74c9a88a
timwlee <timothyl@adobe.com>
2025-04-02 13:09:20 -0700

    Improve LHS

--- blocks/cards/cards.js
+++ blocks/cards/cards.js
@@ -12,7 +12,10 @@ export default function decorate(block) {
     });
     ul.append(li);
   });
-  ul.querySelectorAll('picture > img').forEach((img) => img.closest('picture').replaceWith(createOptimizedPicture(img.src, img.alt, false, [{ width: '750' }])));
+  ul.querySelectorAll('picture > img').forEach((img) => {
+     img.closest('picture').replaceWith(createOptimizedPicture(img.src, img.alt, false, [{ width: '750' }]));
+     img.setAttribute('loading', 'eager');
+  });
   block.textContent = '';
   block.append(ul);
 }
