c961337001ccd516dae5e9d1739160d076ae6000
timwlee <timothyl@adobe.com>
2025-04-02 22:03:19 -0700

    Add featured cards variation

--- blocks/cards/cards.js
+++ blocks/cards/cards.js
@@ -3,7 +3,8 @@ import { createOptimizedPicture } from '../../scripts/aem.js';
 export default function decorate(block) {
   /* change to ul, li */
   const ul = document.createElement('ul');
-  [...block.children].forEach((row) => {
+  const rows = [...block.children];
+  rows.forEach((row) => {
     const li = document.createElement('li');
     while (row.firstElementChild) li.append(row.firstElementChild);
     [...li.children].forEach((div) => {
@@ -12,9 +13,22 @@ export default function decorate(block) {
     });
     ul.append(li);
   });
+
+  // Add class based on number of <li> elements
+  const liCount = ul.children.length;
+  const gridClass = `grid-${Math.min(4, liCount)}`; // Ensure no grid class exceeds grid-4
+
+  if (liCount >= 2 && liCount <= 4) {
+    block.classList.add(gridClass);
+  } else {
+    block.classList.remove('grid-2', 'grid-3', 'grid-4');
+  }
+
+
   ul.querySelectorAll('picture > img').forEach((img) => {
-     img.closest('picture').replaceWith(createOptimizedPicture(img.src, img.alt, false, [{ width: '350' }]));
+    img.closest('picture').replaceWith(createOptimizedPicture(img.src, img.alt, false, [{ width: '350' }]));
   });
+
   block.textContent = '';
   block.append(ul);
 }
